93cd2658b81c675cd9be9109cba9a70d1ccf0b1b
tio <sangvictim@gmail.com>
2024-04-16 16:15:51 +0700

    add(migration): add migration & model route, price

---
+++ app/Models/Price.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Price extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'route_id',
+        'class_id',
+        'price',
+    ];
+}
---
+++ database/migrations/2024_04_16_044050_create_routes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('destination_city')->references('id')->on('cities');
+            $table->foreignId('origin_city')->references('id')->on('cities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('routes');
+    }
+};
