f26a6e6370f29544365dcf52e997a1b274766b23
tio <sangvictim@gmail.com>
2024-05-02 10:51:59 +0700

    add(view): under maintenance

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable implements JWTSubject
         'id',
         'password',
         'remember_token',
+        'email_verified_at',
         'deleted_at'
     ];
 
