fc5d132309427686bb0efad29b371c62dfaa479c
tio <sangvictim@gmail.com>
2024-05-17 16:53:12 +0700

    fix(API): response

--- app/Http/Controllers/ResponseApi.php
+++ app/Http/Controllers/ResponseApi.php
@@ -2,21 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-abstract class ResponseApi
+/**
+ * Response API
+ * @method static \App\Http\Controllers\ApiResponse success(mixed $data = [])
+ */
+class ResponseApi extends JsonResponse
 {
-  public static function success($title, $data, $currentPage = null, $totalPage = null)
+  protected string $title;
+  protected string $message;
+  protected array $errors = [];
+
+  public function __construct(string $message = '', string $title = '')
   {
-    return response()->json(
-      [
-        'code' => Response::HTTP_OK,
-        'message' => 'OK',
-        'title' => $title,
-        'data' => $data,
-        'currentPage' => $currentPage,
-        'totalPage' => $totalPage,
-      ]
-    );
+    $this->title = "OK";
+    $this->message = "Success";
+
+    parent::__construct();
+  }
+
+  public function create(mixed $data = []): static
+  {
+    return static::statusCode(static::HTTP_OK)->data($data);
+  }
+
+  public function success(mixed $data = []): static
+  {
+    return static::create($data);
+  }
+  public function statusCode(int $code): static
+  {
+    return $this->setStatusCode($code);
+  }
+
+  public function message(string $message): static
+  {
+    $this->message = $message;
+    return $this->synchronizeData();
+  }
+
+  public function getMessage(): string
+  {
+    return $this->message;
+  }
+
+  public function title(string $message): static
+  {
+    $this->message = $message;
+    return $this->synchronizeData();
+  }
+
+  public function getTitle(): string
+  {
+    return $this->title;
+  }
+
+  protected function synchronizeData(): static
+  {
+    return static::setData([
+      "title" => $this->getTitle(),
+      "message" => $this->getMessage(),
+      "data" => $this->data,
+      "errors" => $this->errors
+    ]);
+  }
+
+  public function data(mixed $data = []): static
+  {
+    $this->data = $data;
+    return $this->synchronizeData();
   }
 }
--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ResponseApi;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
@@ -15,7 +16,12 @@ class UserController extends Controller
      */
     public function profile()
     {
-        return ResponseApi::success('Profile User', auth()->user());
+        $result = new ResponseApi;
+        $result->message('Profile User');
+        $result->success(auth()->user());
+        return $result;
+        // return $x->success('Profile User', auth()->user());
+        // return ResponseApi::success('Profile User', auth()->user());
     }
 
     /**
@@ -23,8 +29,12 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function notifications(): JsonResponse
+    public function notifications()
     {
-        return ResponseApi::success('Notification User', auth()->user()->notifications);
+        $result = new ResponseApi;
+        $result->message('Notification User');
+        $result->success(auth()->user()->notifications);
+        return $result;
+        // return ResponseApi::success('Notification User', auth()->user()->notifications);
     }
 }
