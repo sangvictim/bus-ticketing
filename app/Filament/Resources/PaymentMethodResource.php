858982de7c34a13cb941df107986b68a17d98a6a
tio <sangvictim@gmail.com>
2024-05-30 16:31:22 +0700

    fix(Module): filter payment method

--- app/Filament/Resources/PaymentMethodResource.php
+++ app/Filament/Resources/PaymentMethodResource.php
@@ -3,10 +3,7 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\PaymentMethodResource\Pages;
-use App\Filament\Resources\PaymentMethodResource\RelationManagers;
-use App\Filament\Resources\PaymentMethodResource\RelationManagers\ParentsRelationManager;
 use App\Models\PaymentMethod;
-use Filament\Forms;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
@@ -17,9 +14,6 @@ use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Livewire\Livewire;
 
 class PaymentMethodResource extends Resource
 {
@@ -69,7 +63,8 @@ class PaymentMethodResource extends Resource
       ->filters([
         SelectFilter::make('parent')->label('Channels')->options(
           PaymentMethod::whereNull('parent')->get()->pluck('name', 'id')->toArray(),
-        )->visible(fn ($livewire) => $livewire->activeTab !== 'parent'),
+        ),
+        // ->visible(fn ($livewire) => $livewire->activeTab == 'Child')
         SelectFilter::make('isActivated')->label('isActivated')->options(
           [
             1 => 'Active',
